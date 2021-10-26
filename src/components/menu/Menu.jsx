d9ab4fab3a0342d734bfd0c347776619059d851f
pkcnj <pilarakoch@gmail.com>
2021-10-26 00:35:51 -0400

    Menu updates

--- src/components/menu/Menu.jsx
+++ src/components/menu/Menu.jsx
@@ -10,9 +10,8 @@ export default function Menu({ menuOpen, setMenuOpen }) {
         <li onClick={()=>setMenuOpen(false)}>
           <a href="#portfolio">Portfolio</a>
         </li>
-        <li onClick={()=>setMenuOpen(false)}>
-          <a href="#work">Work</a>
-        </li>
+
+        
         <li onClick={()=>setMenuOpen(false)}>
           <a href="#contact">Contact</a>
         </li>
